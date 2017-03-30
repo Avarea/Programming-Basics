51e99d4117063558e33c22ee8ff48d7311eb8693
Avarea <valchek@abv.bg>
2017-03-30 20:08:20 +0300

    Homework problems from First Steps in Programming lecture

---
+++ FirstSteps/SquareOfStars/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareOfStars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var n = int.Parse(Console.ReadLine());
+            //Console.WriteLine(new string('*', n));
+            //for (int i = 0; i < n -2; i++)
+            //{
+            //    Console.WriteLine("*" + new string(' ', n-2) + "*");
+            //}
+            //Console.WriteLine(new string('*', n));
+            //======================================================
+            var number = int.Parse(Console.ReadLine());
+            for (int i = 0; i < number; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < number - 2; i++)
+            {
+                Console.WriteLine("*{0}*", new string(' ', number - 2));
+            }
+
+
+            for (int i = 0; i < number; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
