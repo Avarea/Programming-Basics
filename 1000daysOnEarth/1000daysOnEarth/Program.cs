d412960aa8ffbc38b80b106f1883e20b7ff4a71a
Avarea <valchek@abv.bg>
2017-03-30 20:03:22 +0300

    Homework problem - 1000 Days on Earth

---
+++ 1000daysOnEarth/1000daysOnEarth/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1000daysOnEarth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var date = Console.ReadLine();
+            var format = "dd-MM-yyyy";
+            var birthDate = DateTime.ParseExact(date, format, CultureInfo.InvariantCulture);
+            birthDate = birthDate.AddDays(999);
+            Console.WriteLine(birthDate.ToString(format));
+
+        }
+    }
+}
